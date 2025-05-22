35bc6ca2d31902caae83778c60c45f615cf512bb
Ivan Ferreira <ivan.marcelo.ferreira@gmail.com>
2025-05-21 21:26:51 -0300

    add user copy

---
+++ src/users/user.entity copy.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: Number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+}
